f584f558053dcc7e86c94c89345a9d60ad30ac76
Carlos Fernando Torres <cftorres20@gmail.com>
2023-02-23 23:51:05 -0300

    correccion act 2

--- act2-JS/index.js
+++ act2-JS/index.js
@@ -1,9 +1,8 @@
-
-let presentacion = ['Carlos', 30, true, "30/10/1992"]
+let presentacion = ["Carlos", 30, true, "30/10/1992"];
 
 const libro = {
-    titulo: 'La Tregua',
-    autor: 'Mario Benedetti'
-    fecha: 1960,
-    url: 'https://bit.ly/3Iq7nMA',
-}
+  titulo: "La Tregua",
+  autor: "Mario Benedetti",
+  fecha: 1960,
+  url: "https://bit.ly/3Iq7nMA",
+};
