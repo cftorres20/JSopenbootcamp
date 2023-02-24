5c1bbb11d9da05340af399f89a185cf062210b15
Carlos Fernando Torres <cftorres20@gmail.com>
2023-02-23 23:49:36 -0300

    respuesta actividad 2

---
+++ act2-JS/index.js
@@ -0,0 +1,9 @@
+
+let presentacion = ['Carlos', 30, true, "30/10/1992"]
+
+const libro = {
+    titulo: 'La Tregua',
+    autor: 'Mario Benedetti'
+    fecha: 1960,
+    url: 'https://bit.ly/3Iq7nMA',
+}
